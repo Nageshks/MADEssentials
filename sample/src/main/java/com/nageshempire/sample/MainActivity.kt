e08b2b4e4bde350c9c0c72090f5de0cddb1a8455
Nageshks <49113203+Nageshks@users.noreply.github.com>
2022-06-17 20:39:27 +0530

    Initial commit

---
+++ sample/src/main/java/com/nageshempire/sample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nageshempire.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Todo Add View
+    }
+
+}
