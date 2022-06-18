be65f8e6f175ddde3927e6e06983351bfbd27afb
Nagesh <nageshempire@gmail.com>
2022-06-18 11:27:44 +0530

    Credit for flow

--- library/src/main/java/com/nageshempire/mad/essentials/Flow.kt
+++ library/src/main/java/com/nageshempire/mad/essentials/Flow.kt
@@ -3,7 +3,7 @@ package com.nageshempire.mad.essentials
 
 import kotlinx.coroutines.flow.*
 
-
+/** Credit https://proandroiddev.com/from-rxjava-to-kotlin-flow-throttling-ed1778847619#:~:text=In%20Kotlin%20Flow%20though%20there%20is%20no%20such,this:%20fun%20%3CT%3E%20Flow%3CT%3E.throttleFirstJava%20(periodMillis:%20Long):%20Flow%3CT%3E%20%7B */
 fun <T> Flow<T>.throttleFirst(periodMillis: Long = 500): Flow<T> {
     require(periodMillis > 0) { "period should be positive" }
     return flow {
